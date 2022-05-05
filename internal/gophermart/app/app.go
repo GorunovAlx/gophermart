3e5f07daf22b7ce9ff26e1dbf44af542e968faae
Алексей Горюнов <alexeygorunovpub@gmail.com>
2022-05-05 20:28:26 +0300

    check tests

--- internal/gophermart/app/app.go
+++ internal/gophermart/app/app.go
@@ -2,10 +2,10 @@ package app
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/GorunovAlx/gophermart/config"
 	"github.com/GorunovAlx/gophermart/pkg/logger"
-	"github.com/GorunovAlx/gophermart/pkg/postgres"
 	"github.com/rs/zerolog"
 
 	v1 "github.com/GorunovAlx/gophermart/internal/gophermart/controller/http/v1"
@@ -16,21 +16,24 @@ func Run(cfg *config.Config) {
 
 	l.Debug("cfg: runaddress: %v, database: %v, accrual: %v", cfg.RunAddress, cfg.DatabaseURI, cfg.AccrualAddress)
 	// Repository
-	pg, err := postgres.New(cfg)
-	if err != nil {
-		l.Fatal(fmt.Errorf("app - Run - postgres.New: %w", err))
-	}
-	l.Debug("postgres: %v", pg.Pool)
-	defer pg.Close()
-
-	serviceShelf, err := v1.NewServiceShelf(cfg, pg)
+	/*
+		pg, err := postgres.New(cfg)
+		if err != nil {
+			l.Fatal(fmt.Errorf("app - Run - postgres.New: %w", err))
+		}
+		l.Debug("postgres: %v", pg.Pool)
+		defer pg.Close()
+	*/
+	serviceShelf, err := v1.NewServiceShelf(cfg, nil)
 	if err != nil {
 		l.Fatal(fmt.Errorf("app - Run - serviceShelf.New: %w", err))
 	}
 
 	router := v1.Initialize(serviceShelf)
 
-	router.Negroni.Run(cfg.RunAddress)
+	l.Fatal(http.ListenAndServe(cfg.RunAddress, router.Negroni))
+
+	//router.Negroni.Run(cfg.RunAddress)
 
 	/*
 		s := &http.Server{
